b2c6fb34c080e58a6179f2b53a01792bd907596e
Valera <valera@the-developer.ru>
2013-03-28 17:12:49 +0400

    Initial Commit

---
+++ unmountMe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  unmountMe
+//
+//  Created by Valera Antonov on 3/28/13.
+//  Copyright (c) 2013 Valera Antonov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ unmountMe/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  unmountMe
+//
+//  Created by Valera Antonov on 3/28/13.
+//  Copyright (c) 2013 Valera Antonov. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
